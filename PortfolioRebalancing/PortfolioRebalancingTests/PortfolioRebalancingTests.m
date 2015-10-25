01625dac72f1e358646235f4a8bc8c2c3155373a
Jenny Chang Ho <jenny.changho@rbc.com>
2015-10-25 10:18:29 -0400

    removed additional pod files and added a new test case

--- PortfolioRebalancing/PortfolioRebalancingTests/PortfolioRebalancingTests.m
+++ PortfolioRebalancing/PortfolioRebalancingTests/PortfolioRebalancingTests.m
@@ -15,16 +15,6 @@
 
 @implementation PortfolioRebalancingTests
 
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
 - (void)testParsingPortfolioFromFile {
     
     Portfolio *portfolio = [[Portfolio alloc] initWithListOfTickersFile:@"SamplePortfolio"];
@@ -32,6 +22,15 @@
     NSString *result = [portfolio getRebalancedAllocation];
     XCTAssertTrue([result isEqualToString:expected],
                    @"Strings are not equal %@", expected);
+}
+
+- (void)testFinancialsRebalancing {
+    
+    Portfolio *portfolio = [[Portfolio alloc] initWithListOfTickersFile:@"FinancialPortfolio"];
+    NSString *expected = @"sell 101 of CM, buy 243 of RY, sell 155 of BMO, buy 28 of TD";
+    NSString *result = [portfolio getRebalancedAllocation];
+    XCTAssertTrue([result isEqualToString:expected],
+                  @"Strings are not equal %@", expected);
 }
 
 @end
